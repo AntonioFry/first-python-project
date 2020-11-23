248fee539ae303afc92ff6e40ff3b1ea494a9078
Antonio Fry <antoniofry2000@gmail.com>
2020-11-23 10:46:28 -0700

    Add conditionals for the game

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ health = 10
 if age >= 18:
   print("You are old enough!")
 
-  wants_to_play = input("Do you want to play? ")
+  wants_to_play = input("Do you want to play? ").lower()
   if wants_to_play == "yes":
     print("Let's play!")
+
+    left_or_right = input("First choice... (left/righ)? ")
+
+    if left_or_right == 'left':
+      ans = input("You find a bridge. Do you cross it? (yes/no)? ")
+
+      if ans == 'yes':
+        print("The bridge breaks and you die...")
+
+      else:
+        print("You turn around and head back the way you came... good job.")
+
+    else:
+      print("You fell to your death...")
+  
+  else:
+    print("Bye.")
 
 else:
   print("You are not old enough to play.")
