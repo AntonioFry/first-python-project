0251372c50d2857a9d09e859cbef45e129c0e901
Antonio Fry <antoniofry2000@gmail.com>
2020-11-23 10:35:22 -0700

    Add input block and conditional for if age is 18 and above

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ age = int(input("What is your age? "))
 health = 10
 
 if age >= 18:
-  print("You are old enough!"):
+  print("You are old enough!")
+
+  wants_to_play = input("Do you want to play? ")
+  if wants_to_play == "yes":
+    print("Let's play!")
+
 else:
   print("You are not old enough to play.")
 
